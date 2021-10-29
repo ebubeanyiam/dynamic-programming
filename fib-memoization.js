0b444f9c76775b57b31563733e6dd62e222de641
Ebube <dbupanyiam@gmail.com>
2021-10-29 02:44:00 +0100

    Fibonacci sequence challenge

---
+++ fib-memoization.js
@@ -0,0 +1,33 @@
+// Fibonacci Sequence Calculator
+
+// AIM: Get the number at a particular position in the fibonacci sequence.
+
+// number :    1,  2,  3,  4,  5,  6,  7,  8,  9
+// f(number):  1,  1,  2,  3,  5,  8,  13, 21, 34
+
+// Basic Function calculating the number in n position of the fiboacci sequence
+
+// Methodology: Function uses recursion and runs at O(2^n) time which causes it to be sow when taking in large number arguments
+// Recursion
+const fib = (n) => {
+  if (n <= 2) return 1;
+
+  return fib(n - 1) + fib(n - 2);
+};
+
+console.log(fib(50));
+
+// Optimized Function calculating the number in n position of the fiboacci sequence
+
+// Methodology: Function uses recursion and memoization and runs at O(n) time which increased it's calculation speed
+// Recursion + Memoization
+const fibOptimized = (n, memo = {}) => {
+  if (n in memo) return memo[n];
+
+  if (n <= 2) return 1;
+
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
+};
+
+console.log(fibOptimized(50));
